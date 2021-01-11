3bf1b1a94db80f0dbae405852c1e272627465c65
Tyrell <tyrellscott01@gmail.com>
2021-01-11 16:22:54 -0700

    Oculus Integration

---
+++ Growler_Repair_Sim/Assets/Scripts/UI_Interaction/UI_ScreenTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ScreenTransition : MonoBehaviour
+{
+
+    public GameObject[] canvasToTurnOn;
+    //public GameObject canvasToTurnOff;
+    public GameObject[] multiCanvasTurnOff;
+    
+
+ /// <summary>
+ /// Allows the transition of canvases on button click
+ /// </summary>
+    public void EnableUI()
+    {
+        foreach (var item in canvasToTurnOn)
+        {
+            item.SetActive(true);
+        }
+
+        foreach (var item in multiCanvasTurnOff)
+        {
+            item.SetActive(false);
+        }
+
+
+
+    }
+}
