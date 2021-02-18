1220c1a150a21188181e71b1bc7eda9e2fb39638
Lillian Wright <10803934@my.uvu.edu>
2021-02-18 11:22:29 -0700

    gitflow-feature-stash: Audio

---
+++ Growler_Repair_Sim/Assets/Scripts/Audio/VolumeLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+
+public class VolumeLevel : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetVolume(float sliderValue)
+    {
+        mixer.SetFloat("AudioVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
