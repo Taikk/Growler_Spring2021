45be489ae055805498af107185e3263963a89c14
Lillian Wright <10803934@my.uvu.edu>
2021-03-19 10:57:31 -0600

    LOD PseudoCode
    Cause it deleted the other one for some dumb reason. . .

--- Growler_Repair_Sim/Assets/Scripts/LODHighlight.cs
+++ Growler_Repair_Sim/Assets/Scripts/LODHighlight.cs
@@ -1,7 +1,20 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LODHighlight : MonoBehaviour
 {
-    public class LODHighlight
-    {
-        
-    }
+    //public GameObject XRRig
+    //public GameObject highlighter
+    
+    //public function to determine if XRRig is in bounding box of object group
+    //public void BoundingBoxFind()
+        //if XRRig.position == gameObject.GetComponent<Collider>().bounds
+            //SetGroup();
+        //else
+            //keep set scan range group as is
+    
+    //public function to set bounding box group and enable Highlight interactions with objects of that group
+    //public void SetGroup()
+        //call all the objects within the bounding box group
+        //enable their highlight interaction functions
 }
